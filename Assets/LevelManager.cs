41b5e82dc07c97b57396d7453fcef0d654f08210
Jbaker8390 <jonathantuan02@gmail.com>
2018-02-05 20:46:31 -0800

    added logic to the button to log output to console, also added button to Game scene to return back to start scene

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
-	
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Lvel load requested for : " + name);
+        //Loads the particular level
+        Application.LoadLevel(name);
+    }
 		
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+    }
+
+    public void ReturnToStart()
+    {
+        Application.LoadLevel("Start");
+    }
 	}
-}
